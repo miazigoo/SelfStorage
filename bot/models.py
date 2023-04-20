538e1392f2aa2ad5e49ccc7a5f2c455488ddc1d5
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-04-20 13:34:04 +0300

    Simplify db scheme

--- bot/models.py
+++ bot/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Client(models.Model):
     chat_id = models.CharField(max_length=100, verbose_name='ID чата клиента')
-    nickname = models.CharField(max_length=500, verbose_name='Никнейм пользователя')
-    name = models.CharField(max_length=40, verbose_name='Имя пользователя', null=True, blank=True)
+    nickname = models.CharField(max_length=500, verbose_name='Никнейм клиента')
+    name = models.CharField(max_length=40, verbose_name='Имя клиента', null=True, blank=True)
     tel_number = models.CharField(max_length=12, verbose_name='Номер телефона', null=True, blank=True, unique=True)
     email = models.EmailField(max_length=200, verbose_name='E-mail', blank=True, null=True, unique=True)
     created_at = models.DateTimeField(verbose_name='Создано', auto_now_add=True)
@@ -21,20 +21,6 @@ class Client(models.Model):
         verbose_name_plural = 'Клиенты'
 
 
-class ClientAddress(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE,
-                               related_name='addresses', verbose_name='Клиент',
-                               null=True, blank=True)
-    address = models.TextField()
-
-    def __str__(self):
-        return f'{self.address} {self.client}'
-
-    class Meta:
-        verbose_name = 'Адрес клиента'
-        verbose_name_plural = 'Адреса клиентов'
-
-
 class Storage(models.Model):
     name = models.CharField(max_length=25, verbose_name='Название склада')
     address = models.TextField()
@@ -43,8 +29,8 @@ class Storage(models.Model):
         return f'{self.name}'
 
     class Meta:
-        verbose_name = 'Адрес склада'
-        verbose_name_plural = 'Адреса складов'
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
 
 
 class Box(models.Model):
@@ -55,42 +41,44 @@ class Box(models.Model):
     height = models.FloatField()
 
     def __str__(self):
-        return f'{self.name}({self.length}x{self.width}x{self.height})'
+        return f'{self.name}({self.length}x{self.width}x{self.height} м)'
 
     class Meta:
         verbose_name = 'Бокс'
         verbose_name_plural = 'Боксы'
 
 
-class BoxUsage(models.Model):
-    box = models.ForeignKey(Box, on_delete=models.CASCADE, related_name='usages', verbose_name='Номер бокса')
-    start_date = models.DateField(auto_now_add=True, verbose_name='Открыто')
-    end_date = models.DateField(verbose_name='Закрыто')
-
-    def __str__(self):
-        return {self.box}
-
-
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='orders', verbose_name='Клиент')
     created_at = models.DateTimeField(verbose_name='Создано', auto_now_add=True)
-    box_usages = models.ManyToManyField(BoxUsage, through='BoxUsageByOrders', related_name='orders',
-                                        verbose_name='Используемые боксы')
+    box = models.ForeignKey(Box, on_delete=models.CASCADE, related_name='orders',
+                            verbose_name='Бокс', null=True, blank=True)
+    start_storage_date = models.DateField(auto_now_add=True, verbose_name='Открыто', null=True, blank=True)
+    end_storage_date = models.DateField(verbose_name='Закрыто', null=True, blank=True)
+    paid_up_to = models.DateField(verbose_name="Оплачено до", null=True, blank=True)
+    weight = models.CharField(max_length=50, verbose_name='Вес', null=True, blank=True)
+    size = models.CharField(max_length=50, verbose_name='Размер', null=True, blank=True)
+    things = models.TextField(max_length=50, verbose_name='Список вещей', null=True, blank=True)
+
+    def get_description(self):
+        if self.things:
+            things = self.things
+        else:
+            things = "Список вещей не был указан"
+        return f"""
+        {things}
+        
+        общий вес: {self.weight}
+        
+        общий размер: {self.size}
+        """
 
     def __str__(self):
-        return f'#{self.pk} {self.client} {self.box_usages}'
+        return f'#{self.pk} {self.client} {self.box}'
 
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
-
-
-class BoxUsageByOrders(models.Model):
-    box_usage = models.ForeignKey(BoxUsage, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.box_usage} {self.order}'
 
 
 class DeliveryStatus(models.Model):
@@ -114,10 +102,6 @@ class Delivery(models.Model):
     type = models.CharField(max_length=10, choices=delivery_types, verbose_name='Тип доставки')
     order = models.ForeignKey(Order, on_delete=models.CASCADE,
                               related_name='deliveries', verbose_name='Заказ')
-    address = models.ForeignKey(ClientAddress, on_delete=models.CASCADE,
-                                related_name='deliveries', verbose_name='Адрес доставки')
-    storage = models.ForeignKey(Storage, on_delete=models.CASCADE,
-                                related_name='deliveries', verbose_name='Адрес склада')
     status = models.ForeignKey(DeliveryStatus, on_delete=models.CASCADE,
                                related_name='deliveries', verbose_name='Статус')
     take_at = models.DateTimeField(verbose_name='Забрать в', null=True, blank=True)
@@ -125,27 +109,11 @@ class Delivery(models.Model):
     deliver_at = models.DateTimeField(verbose_name='Доставить в', null=True, blank=True)
     delivered_at = models.DateTimeField(verbose_name='Доставили в', null=True, blank=True)
     need_measurement = models.BooleanField(verbose_name='Требуются замеры')
+    created_at = models.DateTimeField(verbose_name='Создано', auto_now_add=True)
 
     def __str__(self):
-        if self.type == 'in':
-            return f'#{self.pk}:{self.address}-{self.storage} {self.type}'
-        elif self.type == 'out':
-            return f'#{self.pk}:{self.storage}-{self.address} {self.type}'
+        return f'#{self.pk}:{self.order} {self.type}'
 
     class Meta:
         verbose_name = 'Доставка'
         verbose_name_plural = 'Доставки'
-
-
-class Payment(models.Model):
-    box_usage_by_order = models.ForeignKey(BoxUsageByOrders, on_delete=models.CASCADE,
-                                           verbose_name='Предмет платежа', related_name='payments')
-    paid_days = models.IntegerField(verbose_name='Количество оплаченных дней')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата фиксации')
-
-    def __str__(self):
-        return f'#{self.pk} {self.box_usage_by_order} {self.paid_days} {self.created_at}'
-
-    class Meta:
-        verbose_name = 'Платеж'
-        verbose_name_plural = 'Платежи'
