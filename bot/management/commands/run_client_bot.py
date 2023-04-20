e033938d62ab152aa2533f66ba57501a0c4c6a74
Bloodov Pavel <kasper6024@gmail.com>
2023-04-20 20:13:42 +0300

    run_client_bot.py и черновик service_storage.py

--- bot/management/commands/run_client_bot.py
+++ bot/management/commands/run_client_bot.py
@@ -1,3 +1,4 @@
+from bot.models import *
 import logging
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -31,6 +32,7 @@ logger = logging.getLogger(__name__)
 FIRST, SECOND = range(2)
 # Данные обратного вызова
 ONE, TWO, THREE, FOUR = range(4)
+
 
 class Command(BaseCommand):
     help = 'Телеграм-бот'
@@ -74,8 +76,8 @@ class Command(BaseCommand):
             query.answer()
             keyboard = [
                 [
-                    InlineKeyboardButton("FAQ_1", callback_data="FAQ_1"),
-                    InlineKeyboardButton("FAQ_2", callback_data="FAQ_2"),
+                    InlineKeyboardButton("План 1", callback_data="FAQ_1"),
+                    InlineKeyboardButton("План 2", callback_data="FAQ_2"),
                     InlineKeyboardButton("Назад", callback_data="to_start"),
                 ]
             ]
@@ -87,33 +89,88 @@ class Command(BaseCommand):
             return 'PLAN'
 
         def faq(update, _):
-            print('faq ')
             query = update.callback_query
-            query.answer()
+            print(query.data)
+
             keyboard = [
                 [
-                    InlineKeyboardButton("FAQ_1", callback_data='FAQ_1'),
-                    InlineKeyboardButton("FAQ_2", callback_data='FAQ_2'),
-                    InlineKeyboardButton("Назад", callback_data="to_start"),
+                    InlineKeyboardButton("Можно хранить", callback_data='FAQ_1'),
+                    InlineKeyboardButton("Нельзя хранить", callback_data='FAQ_2'),
+                    InlineKeyboardButton("Как храним", callback_data="FAQ_3"),
+                ],
+                [
+                    InlineKeyboardButton("Адрес", callback_data='address'),
+                    InlineKeyboardButton("Цены", callback_data='price'),
+                    InlineKeyboardButton("Режим Работы", callback_data="schedule"),
+                ],
+                [
+                    InlineKeyboardButton("Как оформить хранение", callback_data='FAQ_4'),
+                    InlineKeyboardButton("Как забрать вещи", callback_data='FAQ_5'),
+                ],
+                [
+                    InlineKeyboardButton("Контакты", callback_data='contacts'),
+                    InlineKeyboardButton("На главный", callback_data="to_start"),
                 ]
             ]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            query.edit_message_text(
-                text="Выберете интересующий вопрос", reply_markup=reply_markup
-            )
-            return 'GREETINGS'
+
+            query.answer()
+
+            if query.data == 'to_FAQ':
+                query.edit_message_text(
+                    text="Выберете интересующий вопрос", reply_markup=reply_markup
+                )
+
+            if query.data == 'FAQ_1':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'FAQ_2':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'FAQ_3':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'FAQ_4':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'FAQ_5':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'address':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'price':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'schedule':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+            if query.data == 'contacts':
+                query.edit_message_text(
+                    text=f'Выводим  ответ на  {query.data}', reply_markup=reply_markup
+                )
+
+            return 'SHOW_INFO'
 
         def update_profile(update, _):
             query = update.callback_query
             query.answer()
             keyboard = [
                 [
-                    InlineKeyboardButton("Имя", callback_data="Имя"),
-                    InlineKeyboardButton("Телефон", callback_data="Телефон"),
-                    InlineKeyboardButton("Email", callback_data="Email"),
+                    InlineKeyboardButton("Имя", callback_data="update_name"),
+                    InlineKeyboardButton("Телефон", callback_data="update_phone"),
+                    InlineKeyboardButton("Email", callback_data="update_email"),
                 ],
                 [
-                    InlineKeyboardButton("Адрес доставки", callback_data="Адрес доставки"),
+                    InlineKeyboardButton("Адрес доставки", callback_data="update_address"),
                     InlineKeyboardButton("Договор Оферты", callback_data="Договор Оферты"),
                     InlineKeyboardButton("Назад", callback_data="to_start"),
                 ]
@@ -123,6 +180,47 @@ class Command(BaseCommand):
                 text="Выберете интересующий вопрос", reply_markup=reply_markup
             )
             return 'GREETINGS'
+
+        def update_form(update, _):
+            query = update.callback_query
+            query.answer()
+            keyboard = [
+                [
+                    InlineKeyboardButton("Изменить", callback_data="update_field"),
+                    InlineKeyboardButton("Назад", callback_data="to_start"),
+                ]
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            query.edit_message_text(
+                text=f"Обновить {query.data}", reply_markup=reply_markup
+            )
+            return 'GREETINGS'
+
+        def send_field_info(update, _):
+            query = update.callback_query
+            query.edit_message_text(
+                f'Обновялем {query.data} Введите новое значение'
+            )
+            return 'UPDATE'
+
+        def update_field(update, _):
+            user = update.message.from_user
+            chat_instance = update.message.chat.id
+            logger.info("Пользователь %s рассказал: %s", user.first_name, update.message.text)
+            text = update.message.text
+            update.message.reply_text(f'Спасибо! Вы ввели {text}')
+            print(update)
+
+            return 'BACK_TO_GREETINGS'
+
+        def echo(update, context):
+            # добавим в начало полученного сообщения строку 'ECHO: '
+            text = 'ECHO: ' + update.message.text
+            # `update.effective_chat.id` - определяем `id` чата,
+            # откуда прилетело сообщение
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=text)
+            print(update)
 
         def cancel(update, _):
             # определяем пользователя
@@ -142,25 +240,38 @@ class Command(BaseCommand):
             states={
                 'GREETINGS': [
                     CallbackQueryHandler(choose_plan, pattern='^' + str(ONE) + '$'),
-                    CallbackQueryHandler(faq, pattern='^' + 'to_FAQ' + '$'),
-                    CallbackQueryHandler(start_conversation, pattern='^' + 'to_start' + '$'),
-                    CallbackQueryHandler(update_profile, pattern='^' + 'to_profile' + '$'),
+                    CallbackQueryHandler(faq, pattern='to_FAQ'),
+                    CallbackQueryHandler(start_conversation, pattern='to_start'),
+                    CallbackQueryHandler(update_profile, pattern='to_profile'),
+                    CallbackQueryHandler(update_form, pattern='(update_name|update_phone|update_email|update_address)'),
+                    CallbackQueryHandler(send_field_info, pattern='update_field'),
+                ],
+                'UPDATE': [
+                    MessageHandler(Filters.text & ~Filters.command, update_field)
+                ],
+                'BACK_TO_GREETINGS': [
+                    CommandHandler('start', start_conversation)
+                ],
+                'SHOW_INFO': [
+                    CallbackQueryHandler(faq,
+                                         pattern='(FAQ_1|FAQ_2|FAQ_3|FAQ_4|FAQ_5|address|price|schedule|contacts)'),
+                    CallbackQueryHandler(start_conversation, pattern='to_start'),
                 ]
             },
-            fallbacks = [CommandHandler('cancel', cancel)]
+            fallbacks=[CommandHandler('cancel', cancel)]
         )
 
         dispatcher.add_handler(conv_handler)
+
+        # echo_handler = MessageHandler(Filters.text, echo)
+        # dispatcher.add_handler(echo_handler)
 
         updater.start_polling()
         updater.idle()
 
 
-
-
 if __name__ == '__main__':
     env = environs.Env()
     env.read_env()
-
 
     print('olol')
